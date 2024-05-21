e8f8dfc316bd90be45edcd022bc33ec798d47bd5
SAURETMathieu <mathieu.sauret@oclock.school>
2024-05-21 12:23:23 +0200

    style(button): add success variant for button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,13 +8,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/80",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline:
           "border border-input hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        success:
+          "bg-green-300 text-green-900 hover:bg-green-300/80 border border-green-900",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "underline-offset-4 hover:underline text-primary",
       },
