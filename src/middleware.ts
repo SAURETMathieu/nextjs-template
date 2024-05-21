3a3bfb9b3ff8855b1fc8b8e356b700d4ea640e1d
SAURETMathieu <mathieu.sauret@oclock.school>
2024-05-21 12:21:13 +0200

    feat(middleware): add default values + matcher paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
 import createMiddleware from "next-intl/middleware";
-
 import { localePrefix, locales } from "./navigation";
 
 export default createMiddleware({
-  localePrefix,
-  locales,
+  localePrefix: localePrefix || 'always',
+  locales: locales || ['fr', 'en'],
   defaultLocale: "fr",
 });
 
 export const config = {
-  matcher: ["/", "/(fr|en)/:path*"],
+  matcher: [
+    '/',
+    '/:locale(fr|en)',
+    '/:locale(fr|en):path*',
+    '/:locale(fr|en)/:path*',
+  ],
 };
