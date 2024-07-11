62105953b6511050b1a0c39b91919338f6615624
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-11 17:18:13 +0200

    fix(ToggleMenu): close after click on link

--- src/components/header/ToggleMenu.tsx
+++ src/components/header/ToggleMenu.tsx
@@ -1,15 +1,24 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+"use client";
+
+import { Button } from "@/src/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/src/components/ui/sheet";
+import NavigationLink from "@/components/link/NavigationLink";
 import { Icons } from "@/src/icons/icons";
 import { PanelLeft } from "lucide-react";
+import { useState } from "react";
 
-import { MainNavProps } from "@/types/nav";
 import { siteConfig } from "@/config/site";
+import { MainNavProps } from "@/types/nav";
 
 export default function ToggleMenu({ items }: MainNavProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <Button size="icon" variant="outline" className="sm:hidden">
           <PanelLeft className="size-5" />
@@ -18,21 +27,26 @@ export default function ToggleMenu({ items }: MainNavProps) {
       </SheetTrigger>
       <SheetContent side="left" className="sm:max-w-xs">
         <nav className="grid gap-6 pt-6 text-lg font-medium">
-          <Link href="/" className="flex items-center justify-center">
+          <NavigationLink
+            href="/"
+            className="is-logo flex items-center justify-center"
+            onClick={handleLinkClick}
+          >
             <Icons.logo className="size-20" />
             <span className="inline-block font-bold">{siteConfig.name}</span>
-          </Link>
+          </NavigationLink>
           {items?.map(
             (item) =>
               item.href && (
-                <Link
+                <NavigationLink
                   key={item.title}
                   href={item.href}
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  onClick={handleLinkClick}
                 >
                   {item.icon}
                   {item.title}
-                </Link>
+                </NavigationLink>
               )
           )}
         </nav>
