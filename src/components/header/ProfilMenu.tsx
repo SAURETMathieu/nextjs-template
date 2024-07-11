17c578c8a1741959831cc5fcbcdcc63c5b22782b
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-11 18:01:53 +0200

    feat(ProfilMenu): dynamize i18n

--- src/components/header/ProfilMenu.tsx
+++ src/components/header/ProfilMenu.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import profilePlaceholder from "@/public/placeholder-user.jpg";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,14 +7,60 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@/src/components/ui/dropdown-menu";
+import {
+  getTranslations,
+  getLocale
+} from "next-intl/server";
 
 import { Button } from "@/components/ui/button";
+import LogOutButton from "@/components/buttons/logoutButton";
 import NavigationLink from "@/components/link/NavigationLink";
-import profilePlaceholder from '@/public/placeholder-user.jpg'
 
-export default function ProfilMenu() {
-  return (
+export default async function ProfilMenu() {
+  const t = await getTranslations({ getLocale, namespace: "Navbar" });
+  //const session = await auth();
+  const session = null;
+
+  //const urlImage: string = session?.user?.image ?? profilePlaceholder;
+  const urlImage = profilePlaceholder;
+  return session ? (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full dark:border-none"
+        >
+          <Image
+            src={urlImage}
+            width={36}
+            height={36}
+            alt={t("avatarAlt")}
+            className="overflow-hidden rounded-full"
+            priority
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>
+          {/* {session ? session.user?.email : "email@gmail.com"} */}
+          {session ? "" : "email@gmail.com"}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <NavigationLink href="/profile" aria-label={t("ariaProfile")}>
+          <DropdownMenuItem>{t("profileLinkLabel")}</DropdownMenuItem>
+        </NavigationLink>
+        <NavigationLink href="/support" aria-label={t("ariaSupport")}>
+          <DropdownMenuItem>{t("supportLinkLabel")}</DropdownMenuItem>
+        </NavigationLink>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOutButton />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  ) : (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
@@ -25,23 +72,25 @@ export default function ProfilMenu() {
             src={profilePlaceholder}
             width={36}
             height={36}
-            alt="Avatar"
+            alt={t("avatarAlt")}
             className="overflow-hidden rounded-full"
             priority
           />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>{t("menuLabel")}</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <NavigationLink href="/profile" aria-label="Link to profil page">
-          <DropdownMenuItem>Profil</DropdownMenuItem>
-        </NavigationLink>
-        <NavigationLink href="/support" aria-label="Link to support page">
-          <DropdownMenuItem>Support</DropdownMenuItem>
+        <NavigationLink href="/support" aria-label={t("ariaSupport")}>
+          <DropdownMenuItem>{t("supportLinkLabel")}</DropdownMenuItem>
         </NavigationLink>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Logout</DropdownMenuItem>
+        <NavigationLink href="/login" aria-label={t("ariaSignin")}>
+          <DropdownMenuItem>{t("signin")}</DropdownMenuItem>
+        </NavigationLink>
+        <NavigationLink href="/register" aria-label={t("ariaSignup")}>
+          <DropdownMenuItem>{t("signup")}</DropdownMenuItem>
+        </NavigationLink>
       </DropdownMenuContent>
     </DropdownMenu>
   );
