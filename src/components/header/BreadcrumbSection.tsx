76e9b4ce0352f8ce3c3e8dc9941d60ec22b8b956
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-12 12:47:05 +0200

    style(breadcrtumb): bg for current item

--- src/components/header/BreadcrumbSection.tsx
+++ src/components/header/BreadcrumbSection.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
+import { pathnames } from "@/src/config";
 import { usePathname } from "@/src/navigation";
 import { Home } from "lucide-react";
-import { pathnames } from "@/src/config";
 import { useLocale } from "next-intl";
 
 import {
@@ -30,23 +30,29 @@ export default function BreadcrumbSection() {
   const currentPathNames: string[] = path.split("/").filter((p) => p);
 
   // Translate the path names and remove slashes
-  const currentPathNamesTranslated = currentPathNames?.map((name: string) => {
-    if (pathnames['/' + name as keyof typeof pathnames]) {
-      //@ts-ignore
-      return pathnames['/' + name as keyof typeof pathnames][locale] ?? name;
-    }
-    return name;
-  }).map(name => name.replace(/\//g, ''));
+  const currentPathNamesTranslated = currentPathNames
+    ?.map((name: string) => {
+      if (pathnames[("/" + name) as keyof typeof pathnames]) {
+        //@ts-ignore
+        return (
+          pathnames[("/" + name) as keyof typeof pathnames][locale] ?? name
+        );
+      }
+      return name;
+    })
+    .map((name) => name.replace(/\//g, ""));
 
   console.log(currentPathNamesTranslated);
 
   const maxVisibleItems = 3;
   const invisibleItems =
-  currentPathNamesTranslated.length > maxVisibleItems ? currentPathNamesTranslated.slice(0, -2) : [];
+    currentPathNamesTranslated.length > maxVisibleItems
+      ? currentPathNamesTranslated.slice(0, -2)
+      : [];
 
   // Generate the full path up to the given index
   const generateFullPath = (index: number) => {
-    if (index >= 0 && index < currentPathNames.length){
+    if (index >= 0 && index < currentPathNames.length) {
       return "/" + currentPathNames.slice(0, index + 1).join("/");
     }
     return "/";
@@ -55,7 +61,7 @@ export default function BreadcrumbSection() {
   return (
     <Breadcrumb className="hidden sm:flex">
       <BreadcrumbList>
-      {/* Render the home icon if there are path names */}
+        {/* Render the home icon if there are path names */}
         {currentPathNamesTranslated.length > 0 && (
           <>
             <BreadcrumbItem>
@@ -79,7 +85,6 @@ export default function BreadcrumbSection() {
                 <DropdownMenuContent align="start">
                   {invisibleItems.map((name, index) => {
                     const href = generateFullPath(index);
-                    console.log(href);
                     return (
                       <DropdownMenuItem key={index}>
                         <BreadcrumbLink href={href} className="w-full">
@@ -96,26 +101,31 @@ export default function BreadcrumbSection() {
         )}
 
         {/* Render the rest of visible items */}
-        {currentPathNamesTranslated.slice(invisibleItems.length).map((name, index) => {
-          const actualIndex = invisibleItems.length + index;
-          const href = generateFullPath(actualIndex);
-          console.log(href);
-          const isLastItem =
-            index === currentPathNamesTranslated.slice(invisibleItems.length).length - 1;
+        {currentPathNamesTranslated
+          .slice(invisibleItems.length)
+          .map((name, index) => {
+            const actualIndex = invisibleItems.length + index;
+            const href = generateFullPath(actualIndex);
+            const isLastItem =
+              index ===
+              currentPathNamesTranslated.slice(invisibleItems.length).length -
+                1;
 
-          return (
-            <React.Fragment key={index}>
-              {index > 0 && <BreadcrumbSeparator />}
-              <BreadcrumbItem>
-                {isLastItem ? (
-                  <BreadcrumbPage>{name}</BreadcrumbPage>
-                ) : (
-                  <BreadcrumbLink href={href}>{name}</BreadcrumbLink>
-                )}
-              </BreadcrumbItem>
-            </React.Fragment>
-          );
-        })}
+            return (
+              <React.Fragment key={index}>
+                {index > 0 && <BreadcrumbSeparator />}
+                <BreadcrumbItem>
+                  {isLastItem ? (
+                    <BreadcrumbPage className="rounded bg-foreground/30 p-1 font-bold">
+                      {name}
+                    </BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink href={href}>{name}</BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+              </React.Fragment>
+            );
+          })}
       </BreadcrumbList>
     </Breadcrumb>
   );
